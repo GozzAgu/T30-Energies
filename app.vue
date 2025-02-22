93ddbbc568366ddc24d04fcebd71ab770a507ff3
GozzAgu <gozieagu1@gmail.com>
2025-02-22 16:00:58 +0000

    update

--- app.vue
+++ app.vue
@@ -8,6 +8,7 @@
 useHead({
   title: "T30 Energies - Your Energy Solution",
   meta: [
+    { name: 'google-site-verification', content: 'qdluOiipT70KGJP-XmoQTME6d6nsSjtnlVsdb1eGHFo' },
     { name: "description", content: "T30 Energies provides sustainable and efficient energy solutions for businesses and homes." },
     { name: "keywords", content: "T30 Energies, renewable energy, solar panels, power solutions" },
     { name: "author", content: "T30 Energies" },
