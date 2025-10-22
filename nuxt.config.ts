548674324b05f3cc645a5d68472db901c42f7331
GozzAgu <gozieagu1@gmail.com>
2025-10-22 04:36:48 +0100

    ...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,9 @@ export default defineNuxtConfig({
     }
   },
   modules: ['@nuxt/icon', '@nuxtjs/sitemap', '@nuxtjs/robots'],
+  icon: {
+    collections: ['mdi', 'material-symbols', 'heroicons-outline']
+  },
   css: ['@/assets/css/main.css'],
   postcss: {
     plugins: {
